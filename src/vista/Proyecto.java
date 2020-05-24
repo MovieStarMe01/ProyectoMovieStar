c0a4beee80078063c1b2f8665ec8a2fcb5e7ee9c
Jesús Moisés <Jess Moiss@192.168.1.76>
2020-05-24 17:19:48 -0500

    Mi aporte fue la creación del proyecto iniciando primeramente con la realización grafica del logín y del registro

---
+++ src/vista/Proyecto.java
@@ -0,0 +1,23 @@
+package vista;
+import vista.login; 
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jesús Moisés
+ */
+public class Proyecto{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        login miLogin = new login(null, true);
+        miLogin.setVisible(true);
+    }// fin del main
+    
+}// fin de la clase Proyecto
